e3ddb4f11b1e382ce9cb6d0a764ccfa6b9bf5d60
Samy Zafrany <samy@samyzaf.com>
2017-04-22 22:24:36 +0300

    Update ezsettings.py

--- ezsettings.py
+++ ezsettings.py
@@ -21,32 +21,3 @@ class Settings(dict):
             return self.pop(key)
         else:
             return None
-
-#------------------------------------------------------------
-
-def test1():
-    opt = Settings(aa=11, bb=22, cc='cotton')
-    opt.set('foo', 999)
-    opt.update(bb=22.22, bar='33.33', poo=4.44, zoo=5.55)
-    opt.remove('aa')
-    for key in sorted(opt):
-        print("%s = %s" % (key,opt[key]))
-
-def test2():
-    d = dict(aa=11, bb=22, cc='cotton')
-    opt = Settings(d)
-    opt.set('foo', 999)
-    for key in sorted(opt):
-        print("%s = %s" % (key,opt[key]))
-
-def test3():
-    def print_opt(**opt):
-        for key in sorted(opt):
-            print("%s = %s" % (key,opt[key]))
-
-    opt = Settings(aa=11, bb=22, cc='cotton')
-    print_opt(**opt)
-
-if __name__ == '__main__':
-    test3()
-

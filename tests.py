e76211f0a91462b1c60ff077b97ebc3662c703e1
Samy Zafrany <samy@samyzaf.com>
2017-04-25 04:48:46 +0300

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,27 +1,36 @@
 from ezsettings import Settings
 
 def test1():
-    opt = Settings(aa=11, bb=22, cc='cotton')
+    opt = Settings(aa=11, bb=22, cc='casandra')
     opt.set('foo', 999)
-    opt.update(bb=22.35, bar='33.45', poo=4.56, zoo=5.67)
-    opt.remove('aa')
+    opt.update(bb=22.22, bar='33.33', poo=4.44, zoo=5.55)
+    print("opt.aa =", opt.aa)
+    print("opt.cc =", opt.cc)
+    print(dir(opt))
+    opt.pop('aa')
     for key in sorted(opt):
-        print("%s = %s" % (key,opt[key]))
+        print("Value of %s is: %s" % (key, opt.get(key)))
 
 def test2():
-    d = dict(aa=11, bb=22, cc='cotton')
+    d = dict(aa=11, bb=22, cc='casandra')
     opt = Settings(d)
     opt.set('foo', 999)
+    opt['bar'] = 'baracuda'
+    print(opt)
     for key in sorted(opt):
-        print("%s = %s" % (key,opt[key]))
+        print("Value of %s is: %s" % (key, opt[key]))
 
 def test3():
-    def print_opt(**opt):
-        for key in sorted(opt):
-            print("%s = %s" % (key,opt[key]))
+    d = dict(aa=11, bb=22, cc='casandra')
+    opt = Settings(d, dd='dothan', ee=55)
+    for key in sorted(opt):
+        print("%s = %s" % (key, opt.get(key)))
 
-    opt = Settings(aa=11, bb=22, cc='cotton')
-    print_opt(**opt)
+def test4():
+    d = dict(aa=11, bb=22, cc='casandra')
+    opt = Settings(d, dd='dothan', ee=55)
+    print(opt)
 
 if __name__ == '__main__':
     test1()
+
